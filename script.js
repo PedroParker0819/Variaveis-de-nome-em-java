ae953642ec22d45d399fa8490310ab7c5c702c07
Mono Gótica <113735545+PedroParker0819@users.noreply.github.com>
2023-06-18 15:20:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+var name = "Parker";
+var age = 16;
+var address = "123 Rua Ajenor";
+
+console.log("Meu nome é " + name + ".");
+console.log("Eu tenho " + age + " anos.");
+console.log("Eu moro em " + address + ".");
+
+var name = prompt("Qual seu nome?");
+var age = prompt("Quatos anos voçe tem?");
+
+console.log("Olá, " + name + "Voce tem " + age + " anos.");
+
+var fruit = "fruta favorita"; console.log(fruit);
+
+var frase = "sua escreva uma frase"; console.log(fruit);
